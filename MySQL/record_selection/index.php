ce13e191042a938e63fc29851dfac2e80dc7cdb2
konstantin-bogdan <konsotantin@gmail.com>
2022-02-02 22:27:36 +0200

    id_login

--- MySQL/record_selection/index.php
+++ MySQL/record_selection/index.php
@@ -16,12 +16,17 @@
 	$mysqli = new mysqli ("localhost", "root", "", "test_base");
 	$mysqli->query("SET NAMES 'utf8'");
 
-	$result_set = $mysqli->query ("SELECT * FROM `users`");
+	$result_set = $mysqli->query ("SELECT * FROM `users`"); // виведуться всі
 	printResult ($result_set);
 
-	$result_set = $mysqli->query ("SELECT `id` FROM `users`");
+	$result_set = $mysqli->query ("SELECT `id` FROM `users`"); // виведеться id
 	printResult ($result_set);
 
+	$result_set = $mysqli->query ("SELECT `id`, `login` FROM `users`"); // виведеться id , login
+	printResult ($result_set);
+
+	$result_set = $mysqli->query ("SELECT `id`, `login` FROM `users` WHERE  `id` > 7"); // виведеться id > 7
+	printResult ($result_set);
 
 	var_dump ($mysqli);
 	$mysqli->close();
